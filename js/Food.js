4b09d5be1c4fc6931632c81abeacea02f923c5f2
AshazKhan <73424230+AshazKhan@users.noreply.github.com>
2021-04-19 17:19:34 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,35 @@
+class Food{
+    constructor(){
+       this.foodStock-0
+        this.image = loadImage("images/Milk.png");
+
+    }
+    display(){
+        imageMode(CENTER);
+        image(this.image,200,400,500,500)
+        if (condition) {
+            
+        } else {
+            
+        }
+
+
+
+    }
+    getFoodStock(){
+
+
+
+
+    }
+    updateFoodStock(){
+
+
+
+    }
+    deductFood(){
+
+
+
+    }
+}
